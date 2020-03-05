51b757c715838b9b738911878f4d5d675e6dabb4
Spheya <33225496+Spheya@users.noreply.github.com>
2020-03-05 20:24:49 +0100

    ...and to fix some other stuff

--- EntityComponentSystem/OpenGLRenderer/RenderSystem.cpp
+++ EntityComponentSystem/OpenGLRenderer/RenderSystem.cpp
@@ -1,6 +1,6 @@
 #include "RenderSystem.hpp"
 
-void renderer::RenderSystem::onUpdate(float, const ecs::EntityGroup<EntityData>&, ecs::ChangeBuffer&) {
+void renderer::RenderSystem::onUpdate(float, const ecs::EntityGroup<EntityData>& entityGroup, ecs::ChangeBuffer&) {
 	std::unordered_map<GLenum, bool> enabled;
 
 	for (auto& entity : entityGroup) {
